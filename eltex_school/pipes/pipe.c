4933ea27cc9cb26f443d9d56a180c7f1b4a4871c
Pavel Polyakov <pasha.u.polyak@gmail.com>
2018-03-03 01:27:55 +0700

    sigaction, wait
    Summary:
    	Added wait(0) that allows correctly complete processes;
    	Set signal handler for SIGCHLD.

--- eltex_school/pipes/pipe.c
+++ eltex_school/pipes/pipe.c
@@ -1,16 +1,49 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void sighandler(int sig)
+{
+	printf("[sighandler]: catched %d\n", sig);
+	wait(0);
+}
+
+int execute(char const *file, char *argv[])
+{
+	pid_t pid;
+	pid = fork();
+	if (pid == -1) {
+		perror("[fork]");
+		return -1;
+	}
+	if (pid == 0) { // потомок
+		if (execvp(file, argv) == -1) {
+			perror("[execvp]");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return pid;
+}
 
 int main(int argc, char *argv[])
 {
 	if (argc < 2) {
-		printf("usage: %s <command> [argument]\n", argv[0]);
+		printf("Usage: %s <command> [argument]\n", argv[0]);
 		exit(EXIT_SUCCESS);
 	}
 
-	if (execvp(argv[1], &argv[1]) == -1) {
-		perror("[execvp]");
-		exit(EXIT_FAILURE);
+	struct sigaction act;
+	act.sa_handler = sighandler;
+	sigaction(SIGCHLD, &act, NULL);
+
+	pid_t pid = execute(argv[1], &argv[1]);
+	
+	if (pid > 0) {
+		printf("Child created with %d pid.\n", pid);
 	}
+	wait(0);
+	return 0;
 }
